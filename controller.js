0326f88344e28e0b462c2a301ffed8f6fa4db5c7
cecileanne <cecileannesison@gmail.com>
2019-11-03 19:52:58 -0600

    commented out console logs

--- controller.js
+++ controller.js
@@ -10,23 +10,17 @@ module.exports = function(app) {
 
   // adds new burger
   app.post("/api", function(req, res, next) {
-    console.log(`body ${req.body}`);
-    console.log(JSON.stringify(req.body));
+    // console.log(`body ${req.body}`);
+    // console.log(JSON.stringify(req.body));
     burger.cookNew(req.body.burgerStyle, function(data) {
       res.json(data);
     });
-    /*
-      .then(
-        //   res.redirect("/");
-        res.render("index", { burgers: data })
-      );
-      */
   });
+
   // moves burgers to the devoured column
   app.put("/api/:id", function(req, res) {
     burger.devourOne(req.params.id, function(data) {
       res.json(data);
-      // res.redirect("/");
     });
   });
 }; // closes module exports
