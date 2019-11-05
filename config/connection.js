d9c83823f1c3babd1d128e8260a7bfed7827e6a5
cecileanne <cecileannesison@gmail.com>
2019-11-04 21:54:31 -0600

    added jawsdb to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.password,
-  database: "burgerMenu_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.password,
+    database: "burgerMenu_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
